35cdca7ed1d9323f997a63e81919a23fd0536296
NC4L4NGL4NG <calanglang.nea@obmontessori.edu.ph>
2024-12-10 08:00:36 +0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+ <script>
+        document.getElementById("redirect-form").addEventListener("submit", function (event) {
+            event.preventDefault(); // Prevent the default form submission
+            window.location.href = "https://example.com/success-page.html"; // Redirect URL
+        });
+    </script>
